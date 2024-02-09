e2421912ce676badfc09d30fa942550fd24e3781
RazvanBuzincu <razvanbuzincu@gmail.com>
2024-02-09 09:35:33 -0600

    first commit

---
+++ rangers_shop/helpers.py
@@ -0,0 +1,42 @@
+import decimal
+import requests
+import requests_cache
+import json
+
+
+# setup our api cache location(this will be a temporary database storing our api calls)
+requests_cache.install_cache('image_cache', backend='sqlite')
+
+
+def get_image(search):
+    # 4 parts to every api
+    # url required 
+    # queries/parameters optional
+    # headers/authorization optional
+    # body/postin optional
+
+    url = "https://google-search72.p.rapidapi.com/imagesearch"
+
+    querystring = {"q": search,"gl":"us","lr":"lang_en","num":"10","start":"0"}
+
+    headers = {
+        "X-RapidAPI-Key": "d18640d5c3msh4b7d0dbe1530c4cp1f097fjsncbb8d6552706",
+        "X-RapidAPI-Host": "google-search72.p.rapidapi.com"
+    }
+
+    response = requests.get(url, headers=headers, params=querystring)
+
+    data = response.json()
+
+    img_url = ""
+
+    if 'items' in data.keys():
+           img_url = data['items'][0]['originalImageUrl'] 
+
+    return img_url
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, decimal.Decimal): #if the object is a decimal we are going to encode it 
+                return str(obj)
+        return json.JSONEncoder(JSONEncoder, self).default(obj) #if not the JSONEncoder from json class can handle it
